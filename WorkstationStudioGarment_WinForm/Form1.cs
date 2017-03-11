2e8f15cc7a3d07332972113a4a6930ea23b7f94c
T-Midnight <belousovatanya1995@yandex.ru>
2017-03-11 22:18:58 +0400

    dfgdfg

--- WorkstationStudioGarment_WinForm/Form1.cs
+++ WorkstationStudioGarment_WinForm/Form1.cs
@@ -7,14 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorkstationStudioGarment_WinForm.forms;
 
 namespace WorkstationStudioGarment_WinForm
 {
     public partial class Form1 : Form
     {
+        FTanya f = new FTanya();
         public Form1()
         {
             InitializeComponent();
+
+            f.ShowDialog();
+
         }
     }
 }
---
+++ WorkstationStudioGarment_WinForm/control/ClientControl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkstationStudioGarment_WinForm.manager;
+
+namespace WorkstationStudioGarment_WinForm.control
+{
+    class ClientControl
+    {
+        //public CLIENT client = new CLIENT();
+
+        
+        /// <summary>
+        /// Add clients into CLIENT table 
+        /// </summary>
+        public void addClients(int id_client, string login, string password,
+                                int accsess_level, string surname, string name, string patronymic,
+                                string mail, string phone_number, int sex, int growth, int chest,
+                                int waist, int hip) {
+            try { 
+                using(StudioDB db = new StudioDB()) {
+                    CLIENT client = new CLIENT();
+                    
+                }
+            }
+        
+        }
+        
+        /// <summary>
+        /// Select all record from CLIENT table
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<CLIENT> selectAll()
+        { 
+            using(StudioDB studiodb = new StudioDB()) {
+                
+                var clientQuery = from cl in studiodb.CLIENTs
+                                  select cl;
+                return clientQuery;
+            } 
+        }
+    }
+}
